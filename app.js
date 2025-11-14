bb79111c544fa27f93684bd846385107a14fbdcb
Bertrand Rock <bertrand.rock@iut-tlse3.fr>
2025-11-14 09:19:06 +0100

    interface admin ok

--- app.js
+++ app.js
@@ -41,10 +41,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'))
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
 // Session
