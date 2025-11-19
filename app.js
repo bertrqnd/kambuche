401574b69d167e037c22af898bfa1642106f051c
Bertrand Rock <bertrand.rock@iut-tlse3.fr>
2025-11-19 20:06:37 +0100

    secure proxy

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
 
+// Trust proxy pour Render/Heroku (permet secure cookies derrière reverse proxy)
+if (process.env.NODE_ENV === 'production') {
+  app.set('trust proxy', 1);
+}
+
 // Helmet avec CSP configurée pour autoriser Cloudinary et TinyMCE
 app.use(
   helmet.contentSecurityPolicy({
