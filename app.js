c1297323818980a2f3731e5472d21e2b730e34a7
Bertrand Rock <bertrand.rock@iut-tlse3.fr>
2025-11-19 20:16:20 +0100

    debug auth

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.use(session({
   cookie: {
     maxAge: 1000 * 60 * 60 * 2, // 2 heures
     httpOnly: true,
-    sameSite: 'strict',
+    sameSite: 'lax', // 'lax' nécessaire pour OAuth (callback depuis Google)
     secure: process.env.NODE_ENV === 'production' // HTTPS en production
   }
 }));
